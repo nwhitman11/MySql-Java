70746af75297765dc126cf9a5be05902216a30ae
nwhitman11 <nwhitman11@gmail.com>
2023-01-08 11:09:01 -0600

    First Commit

---
+++ main/java/recipes/Recipes.java
@@ -0,0 +1,12 @@
+package recipes;
+
+import recipes.dao.DbConnection;
+
+public class Recipes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 DbConnection.getConnection();
+	}
+
+}
---
+++ main/java/recipes/exception/DbException.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package recipes.exception;
+
+/**
+ * @author Nathan.Whitman
+ *
+ */
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	public DbException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public DbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public DbException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+}
